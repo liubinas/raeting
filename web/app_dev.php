69b8f112b6653c72657cefe24228875deffead6a
Paulius Podolskis <paulius.p@estina.lt>
2013-08-12 17:25:01 +0300

    Enviromental fixes

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,15 +3,12 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+defined('SYMFONY_ENV') || define('SYMFONY_ENV', (getenv('SYMFONY_ENV') ? getenv('SYMFONY_ENV') : 'dev'));
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+    || SYMFONY_ENV == 'prod'
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -22,7 +19,7 @@ Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel(SYMFONY_ENV, true);
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
