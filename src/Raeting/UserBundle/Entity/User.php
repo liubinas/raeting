fc8fe78223699772a65a2b361358f899f4c686a7
Zilvinas Kuusas <zilvinas@kuusas.lt>
2013-09-04 09:48:55 +0300

    User login fixed

--- src/Raeting/UserBundle/Entity/User.php
+++ src/Raeting/UserBundle/Entity/User.php
@@ -3,13 +3,13 @@
 namespace Raeting\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
 use EstinaCMF\UserBundle\Entity\User as UserBase;
+
 /**
  * User
  *
  * @ORM\Table(name="user")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Raeting\UserBundle\Entity\UserRepository")
  */
 class User extends UserBase
 {
---
+++ src/Raeting/UserBundle/Entity/UserRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Raeting\UserBundle\Entity;
+
+use EstinaCMF\UserBundle\Entity\UserRepository as BaseRepository;
+
+class UserRepository extends BaseRepository
+{
+
+}
