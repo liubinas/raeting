cb3d97756a51ea66183a99c8245be14c3ab2b95a
Marius Blazaitis <marius.b@estina.lt>
2013-10-08 14:07:57 +0300

    signal page new html

--- src/Raeting/RaetingBundle/Resources/views/Signals/show.html.php
+++ src/Raeting/RaetingBundle/Resources/views/Signals/show.html.php
@@ -8,57 +8,88 @@ Signal
 
 <div class="row">
     <div class="col-md-12">
-            <div class="widget box">
-                    <div class="widget-header">
-                            <h4>Signal information</h4>
+        <!-- Tabs-->
+        <div class="tabbable tabbable-custom tabbable-full-width">
+            <div class="tab-content row">
+                <!--=== Overview ===-->
+                <div class="tab-pane active" id="tab_overview">
+                    <div class="fl padd-15">
+                        <div class="list-group">
+                            <li class="list-group-item no-padding">
+                                <img src="https://graph.facebook.com/<?= $entity->getUser()->getFbname() ?>/picture?type=large">
+                            </li>
+                        </div>
                     </div>
-                    <div class="widget-content">
-                            <? if (!empty($entity)): ?>
-                                    <table class="table table-striped table-hover">
-                                        <thead>
-                                        <th>Status</th>
-                                        <th>Symbol</th>
-                                        <th>Buy/Sell</th>
-                                        <th>Open</th>
-                                        <th>Take profit</th>
-                                        <th>Stop loss</th>
-                                        <th>Trader</th>
-                                        <th>Created</th>
-                                        </thead>
-                                        <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <a href="#">
-                                                            <span class="label label-success"><?= $entity->getstatus() ?></span></a>
-                                                    </td>
-                                                    <td><?= $entity->getSymbol()->getTitle() ?></td>
-                                                    <td><?= $entity->getBuyValue() ?></td>
-                                                    <td><?= $entity->getOpen() ?></td>
-                                                    <td><?= $entity->getTakeprofit() ?></td>
-                                                    <td><?= $entity->getStoploss() ?></td>
-                                                    <td>
-                                                        <a href="<?= $view['router']->generate('trader_show', array('slug' => $entity->getUser()->getSlug())) ?>"><?= $entity->getUser()->getFirstname() ?> <?= $entity->getUser()->getLastname() ?></a></td>
-                                                    <td><?= $entity->getCreated()->format('Y-m-d') ?></td>
-                                                </tr>
-                                        </tbody>
-                                    </table>
-                        <? else: ?>
-                            <p><?= $view['translator']->trans('No info') ?></p>
-                        <? endif; ?>
-                    </div>
-            </div>
-            <div class="col-md-12 fb-wrapper">
-                <br/><br/>
-                <div id="fb-root"></div>
-                <script>(function(d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) return;
-                  js = d.createElement(s); js.id = id;
-                  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=133579296820463";
-                  fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));</script>
-                <div class="fb-comments" data-href="<?= $view['router']->generate('signals_show', array('uid' => $entity->getUuid()), true) ?>" data-width="600"></div>
-            </div>
+
+                    <div class="col-md-9">
+                        <div class="row profile-info">
+                            <div class="col-md-12">
+                                <h1><?= $entity->getUser()->getFirstname() ?> <?= $entity->getUser()->getLastname() ?></h1>
+                                <p><?= $entity->getUser()->getAbout() ?></p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="widget box">
+                                    <div class="widget-header">
+                                            <h4>Signal information</h4>
+                                    </div>
+                                    <div class="widget-content">
+                                            <? if (!empty($entity)): ?>
+                                                    <table class="table table-striped table-hover">
+                                                        <thead>
+                                                        <th>Status</th>
+                                                        <th>Symbol</th>
+                                                        <th>Buy/Sell</th>
+                                                        <th>Open</th>
+                                                        <th>Take profit</th>
+                                                        <th>Stop loss</th>
+                                                        <th>Trader</th>
+                                                        <th>Created</th>
+                                                        </thead>
+                                                        <tbody>
+                                                                <tr>
+                                                                    <td>
+                                                                        <a href="#">
+                                                                            <span class="label label-success"><?= $entity->getstatus() ?></span></a>
+                                                                    </td>
+                                                                    <td><?= $entity->getSymbol()->getTitle() ?></td>
+                                                                    <td><?= $entity->getBuyValue() ?></td>
+                                                                    <td><?= $entity->getOpen() ?></td>
+                                                                    <td><?= $entity->getTakeprofit() ?></td>
+                                                                    <td><?= $entity->getStoploss() ?></td>
+                                                                    <td>
+                                                                        <a href="<?= $view['router']->generate('trader_show', array('slug' => $entity->getUser()->getSlug())) ?>"><?= $entity->getUser()->getFirstname() ?> <?= $entity->getUser()->getLastname() ?></a></td>
+                                                                    <td><?= $entity->getCreated()->format('Y-m-d') ?></td>
+                                                                </tr>
+                                                        </tbody>
+                                                    </table>
+                                        <? else: ?>
+                                            <p><?= $view['translator']->trans('No info') ?></p>
+                                        <? endif; ?>
+                                    </div>
+                            </div>
+                            </div>
+                            <!-- /Striped Table -->
+                        </div> <!-- /.row -->
+                        <div class="col-md-12 fb-wrapper">
+                            <div id="fb-root"></div>
+                            <script>(function(d, s, id) {
+                              var js, fjs = d.getElementsByTagName(s)[0];
+                              if (d.getElementById(id)) return;
+                              js = d.createElement(s); js.id = id;
+                              js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=133579296820463";
+                              fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));</script>
+                            <div class="fb-comments" data-href="<?= $view['router']->generate('signals_show', array('uid' => $entity->getUuid()), true) ?>" data-width="600"></div>
+                        </div>
+                    </div> <!-- /.col-md-9 -->
+                </div>
+                <!-- /Overview -->
+            </div> <!-- /.tab-content -->
+        </div>
+        <!--END TABS-->
     </div>
 </div>
 
