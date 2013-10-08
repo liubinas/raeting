1cc1834ab448c221e44da8b2d8eba92c04b6d841
Marius Blazaitis <marius.b@estina.lt>
2013-10-08 13:27:29 +0300

    new index page

--- src/Raeting/RaetingBundle/Resources/views/Raeting/index.html.php
+++ src/Raeting/RaetingBundle/Resources/views/Raeting/index.html.php
@@ -2,48 +2,66 @@
 
 <? $view['slots']->start('header_row') ?>
 
-    Follow and compare trading signals from different markets and traders.
-    Share your ideas easily.
+    Welcome to RAETING
+    
 <? $view['slots']->stop('header_row') ?>
 
 <? $view['slots']->start('content') ?>
 
-    <div class="col-md-12">
-        <div class="widget box">
-            <img src="#" />
-        </div>
-    </div>
-    <div class="row-fluid marketing">
-        
-        <div class="span10 offset1">
-            
-            <div class="row-fluid">
-        
-                <div class="col-sm-4 col-md-4">
-                    <h4>Signals</h4>
-                    <p>
-                        Find best investing ideas and turn them into successful trading decisions.
-                        Share your ideas with the community and compete in the independent global rating.
-                    </p>
+                <hr>
+                <p class="lead">
+                    Follow and compare trading signals from different markets and traders. 
+                    Share your ideas easily.
+                </p>
+                <div class="row row-bg"> <!-- .row-bg -->
+                    <div class="col-sm-8 col-md-4">
+                        <div class="statbox widget box box-shadow">
+                            <div class="widget-content">
+                                <div class="visual cyan">
+                                    <i class="icon-dollar"></i>
+                                </div>
+                                <h4>Signals</h4>
+                                <p>
+                                    Find best investing ideas and turn them into successful trading decisions.
+                                    Share your ideas with the community and compete in the independent global rating.
+                                </p>
+                                <a class="more" href="<?= $view['router']->generate('signals'); ?>">View More <i class="pull-right icon-angle-right"></i></a>
+                            </div>
+                        </div> <!-- /.smallstat -->
+                    </div> <!-- /.col-md-3 -->
+
+                    <div class="col-sm-8 col-md-4">
+                        <div class="statbox widget box box-shadow">
+                            <div class="widget-content">
+                                <div class="visual green">
+                                    <i class="icon-dollar"></i>
+                                </div>
+                                <h4>Traders</h4>
+                                <p>
+                                    Find the best performing and the most interesting signal providers. 
+                                    Share your strategies and compete with others globally.
+                                </p>
+                                <a class="more" href="<?= $view['router']->generate('trader'); ?>">View More <i class="pull-right icon-angle-right"></i></a>
+                            </div>
+                        </div> <!-- /.smallstat -->
+                    </div> <!-- /.col-md-3 -->
+
+                    <div class="col-sm-8 col-md-4 hidden-xs">
+                        <div class="statbox widget box box-shadow">
+                            <div class="widget-content">
+                                <div class="visual yellow">
+                                    <i class="icon-dollar"></i>
+                                </div>
+                                <h4>API</h4>
+                                <p>
+                                    Our system is secure, free and open for integration with your favorite trading 
+                                    platform or tool.
+                                </p>
+                                <!--<a class="more" href="<?= $view['router']->generate('api'); ?>">View More <i class="pull-right icon-angle-right"></i></a>-->
+                            </div>
+                        </div> <!-- /.smallstat -->
+                    </div> <!-- /.col-md-3 -->
+
                 </div>
-                
-                <div class="col-sm-4 col-md-4">
-                    <h4>Traders</h4>
-                    <p>
-                        Find the best performing and the most interesting signal providers. 
-                        Share your strategies and compete with others globally.
-                    </p>
-                </div>
-                <div class="col-sm-4 col-md-4">
-                    <h4>API</h4>
-                    <p>
-                        Our system is secure, free and open for integration with your favorite trading 
-                        platform or tool.
-                    </p>
-                </div>
-            
             </div>
-        
-        </div>
-    </div>
 <? $view['slots']->stop('content') ?>
