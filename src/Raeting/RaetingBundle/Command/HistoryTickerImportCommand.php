5e42f61f07ab90d8281f001a18ffcc8280bb02f3
Marius Blazaitis <marius.b@estina.lt>
2013-11-19 18:08:58 +0200

    ticker history importer by url

--- src/Raeting/RaetingBundle/Command/HistoryTickerImportCommand.php
+++ src/Raeting/RaetingBundle/Command/HistoryTickerImportCommand.php
@@ -27,46 +27,36 @@ class HistoryTickerImportCommand extends ContainerAwareCommand
         $symbolService = $container->get('raetingraeting.service.symbol');
         
         $symbols = $symbolService->getSymbolsForStockImport();
-        $symbolIds = array();
-        if(!empty($symbols)){
-            foreach($symbols as $symbol){
-                $symbolIds[] = $symbol->getSymbol();
-            }
-        }
         
         $objPHPExcel = new \PHPExcel();
-        
-        $files = $container->get('raetingraeting.service.file_management')->scanDir($container->get('kernel')->getRootDir().'/../uploads/history_ticker_rates');
         
         $totalInsertsDone = 0;
         $totalUpdatesDone = 0;
         $filesInserted = 0;
         
-        if(!empty($files)){
-            foreach($files as $file){
-                try {
-                    $insertsFromFile = 0;
-                    $updatesFromFile = 0;
-                    $inputFileType = \PHPExcel_IOFactory::identify($file);
-                    $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
-                    $objPHPExcel = $objReader->load($file);
-                    
-                    $fullFileName = basename($file);
-                    $fileName = substr($fullFileName, 0, strpos($fullFileName, '.'));
-                    
-                    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-                    $query = '';
-                    if(!empty($sheetData)){
-                        foreach($sheetData as $key => $row){
-                            if($key > 1 && $row['A'] >= '2003-01-01'){
-                                
-                                $data = array('symbol'=>$fileName);
-                                $rate = ($row['B']+$row['C']+$row['D']+$row['E'])/4;
+        if(!empty($symbols)){
+            foreach($symbols as $symbol){
+                $url = "http://ichart.yahoo.com/table.csv?s=".$symbol->getSymbol()."&a=0&b=1&c=2003%20&d=8&e=30&f=2099&g=d&ignore=.csv";
+                $csv = file_get_contents($url);
+                $insertsFromFile = 0;
+                $updatesFromFile = 0;
+                
+                $query = '';
+                
+                $rows = explode("\n", $csv);
+                $key = 0;
+                if(!empty($rows)){
+                    foreach($rows as $row){
+                        $row = explode(",", $row);
+                        if(!empty($row[0])){
+                            if($key > 0 && $row[0] >= '2003-01-01'){
+                                $data = array('symbol'=>$symbol->getSymbol());
+                                $rate = ($row[1]+$row[2]+$row[3]+$row[4])/4;
                                 $data['bid'] = $rate;
                                 $data['ask'] = $rate;
-                                $data['high'] = $row['C'];
-                                $data['low'] = $row['D'];
-                                $data['date'] = $row['A'];
+                                $data['high'] = $row[2];
+                                $data['low'] = $row[3];
+                                $data['date'] = $row[0];
                                 $query .= $rateService->getInsertDataQuery($data);
                                 if($query != null && strpos($query, 'INSERT INTO') !== false){
                                     $insertsFromFile++;
@@ -75,18 +65,16 @@ class HistoryTickerImportCommand extends ContainerAwareCommand
                                 }
                             }
                         }
+                        $key++;
                     }
-                    if(!empty($query)){
-                        $rateService->executeQuery($query);
-                    }
-                    $totalInsertsDone += $insertsFromFile;
-                    $totalUpdatesDone += $updatesFromFile;
-                    if($insertsFromFile > 0 || $updatesFromFile > 0){
-                        $container->get('raetingraeting.service.file_management')->moveFile($file, $container->get('kernel')->getRootDir().'/../uploads/imported_history_ticker_rates/'.$fullFileName);
-                        $filesInserted++;
-                    }
-                } catch(Exception $e) {
-                    die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+                }
+                if(!empty($query)){
+                    $rateService->executeQuery($query);
+                }
+                $totalInsertsDone += $insertsFromFile;
+                $totalUpdatesDone += $updatesFromFile;
+                if($insertsFromFile > 0 || $updatesFromFile > 0){
+                    $filesInserted++;
                 }
             }
         }
