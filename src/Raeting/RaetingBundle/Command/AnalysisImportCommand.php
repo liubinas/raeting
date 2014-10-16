3ae5d628ddca136ba4a0a4483ae2271355853672
Mindaugas Liubinas <mindaugas.l@estina.lt>
2014-10-16 23:14:50 +0300

    Update to analysis import

--- src/Raeting/RaetingBundle/Command/AnalysisImportCommand.php
+++ src/Raeting/RaetingBundle/Command/AnalysisImportCommand.php
@@ -1,6 +1,13 @@
 <?php
 namespace Raeting\RaetingBundle\Command;
 
+use Raeting\RaetingBundle\Entity\Analyst as AnalystEntity;
+
+use Raeting\RaetingBundle\Service\FileManagement;
+use Raeting\RaetingBundle\Service\Analysis;
+use Raeting\RaetingBundle\Service\Analyst;
+use Raeting\RaetingBundle\Service\Symbol;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,70 +18,146 @@ class AnalysisImportCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this
-            ->setName('raeting:analysis:import')
-            ->setDescription('Imports analysis')
-        ;
+        $this->setName('raeting:analysis:import')
+             ->setDescription('Imports analysis');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $container = $this->getContainer();
+        /** @var Analysis */
+        $analysisService = $this->getContainer()->get('raetingraeting.service.analysis');
+        /** @var Analyst */
+        $analystService = $this->getContainer()->get('raetingraeting.service.analyst');
+        /** @var FileManagement */
+        $fileManagementService = $this->getContainer()->get('raetingraeting.service.file_management');
+        /** @var string */
+        $sourceDir = dirname($this->getContainer()->get('kernel')->getRootDir()) . '/uploads/analysis';
+        /** @var string */
+        $archiveDir = dirname($this->getContainer()->get('kernel')->getRootDir()) . '/uploads/archive/analysis';
+
+        //$objPHPExcel = new \PHPExcel();
+
+        $files = $fileManagementService->scanDir($sourceDir);
+        if (empty($files)) {
+            $output->writeln(sprintf('<error>No files found under %s</error>', $sourceDir));
+            return;
+        }
+
+        $totalInsertsDone = 0;
+        $filesInserted = 0;
 
         $output->writeln('<info>Importing analysis...</info>');
 
-        $objPHPExcel = new \PHPExcel();
-        
-        $files = $container->get('raetingraeting.service.file_management')->scanDir($container->get('kernel')->getRootDir().'/../uploads/analysis');
-        
-        $analysisService = $container->get('raetingraeting.service.analysis');
-        
-        $totalInsertsDone = 0;
-        $filesInserted = 0;
-        
-        if(!empty($files)){
-            foreach($files as $file){
+        foreach ($files as $file) {
+
+            if (!$this->filenameMatchesSymbol($file)) {
+                $output->writeln(sprintf('<error>File %s does not match any symbol in DB</error>', basename($file)));
+                continue;
+            }
+
+            try {
+                $objPHPExcel = $this->loadFile($file);
+            } catch (\Exception $e) {
+                $output->writeln(sprintf('<error>Error loading file %s, message: %s</error>', $sourceDir, $e->getMessage()));
+                continue;
+            }
+
+            $insertsFromFile = 0;
+
+            $loadedSheetNames = $objPHPExcel->getSheetNames();
+            foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) {
+                $sheetData = $objPHPExcel->getSheet($sheetIndex)->toArray(null,true,true,true);
+
                 try {
-                    $insertsFromFile = 0;
-                    $inputFileType = \PHPExcel_IOFactory::identify($file);
-                    $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
-                    $objReader->setLoadAllSheets();
-                    $objPHPExcel = $objReader->load($file);
-                    
-                    $fullFileName = basename($file);
-                    $fileName = substr($fullFileName, 0, strpos($fullFileName, '.'));
-                    
-                    $loadedSheetNames = $objPHPExcel->getSheetNames();
-                    foreach($loadedSheetNames as $sheetIndex => $loadedSheetName) {
-                        $sheetData = $objPHPExcel->getSheet($sheetIndex)->toArray(null,true,true,true);
-                        if(!empty($sheetData)){
-                            foreach($sheetData as $key => $row){
-                                if($key > 1){
-                                    $data = array('analyst'=>$loadedSheetName, 'ticker'=>$fileName);
-                                    $data['estimation'] = $row['C'];
-                                    $data['period'] = $row['D'];
-                                    $data['date'] = $row['B'];
-                                    $data['recommendation'] = $row['A'];
-                                    if(!empty($data['estimation']) && !empty($data['date'])){
-                                        $analyst = $container->get('raetingraeting.service.analyst')->getByImportSlug($data['analyst']);
-                                        $insertsFromFile += $analysisService->insertData($data, $analyst);
-                                    }
-                                }
-                            }
-                        }
+                    $analyst = $this->extractAnalyst($sheetData);
+                } catch (\InvalidArgumentException $e) {
+                    $output->writeln(sprintf('<comment>No analyst data found in the sheet: %s</comment>', $loadedSheetName));
+                    continue;
+                }
+
+                foreach ($sheetData as $rowNum => $row) {
+                    if ($rowNum < 5) {
+                        continue;
                     }
-                    $totalInsertsDone += $insertsFromFile;
-                    if($insertsFromFile > 0){
-                        $container->get('raetingraeting.service.file_management')->moveFile($file, $container->get('kernel')->getRootDir().'/../uploads/imported_analysis/'.$fullFileName);
-                        $filesInserted++;
+                    /** TODO create analysis */
+                    /**
+                    $data = array(
+                        'analyst'=>$loadedSheetName,
+                        'ticker'=>$fileName
+                    );
+                    $data['recommendation'] = $row['A'];
+                    $data['date']           = $row['B'];
+                    $data['estimation']     = $row['C'];
+                    $data['period']         = $row['D'];
+
+
+
+                    if (!empty($data['estimation']) && !empty($data['date'])) {
+                        $analyst = $analystService->getByImportSlug($data['analyst']);
+                        $insertsFromFile += $analysisService->insertData($data, $analyst);
                     }
-                } catch(Exception $e) {
-                    die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+                    */
                 }
             }
+            $totalInsertsDone += $insertsFromFile;
+            if ($insertsFromFile > 0){
+                $fileManagementService->moveFile($file, $archiveDir . '/' . basename($file));
+                $filesInserted++;
+            }
         }
-        
+
 
         $output->writeln('<info>Files inserted: '.$filesInserted.'.Inserts done: '.$totalInsertsDone.'</info>');
     }
+
+    /**
+     * @param string $file
+     *
+     * @return \PHPExcel
+     */
+    private function loadFile($file)
+    {
+        $inputFileType = \PHPExcel_IOFactory::identify($file);
+        $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
+        $objReader->setLoadAllSheets();
+
+        return $objReader->load($file);
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return bool
+     */
+    private function filenameMatchesSymbol($file)
+    {
+        /** @var Symbol */
+        $symbolService = $this->getContainer()->get('raetingraeting.service.symbol');
+
+        $baseName = basename($file);
+        $name = substr($baseName, 0, strpos($baseName, '.'));
+
+        return (bool) $symbolService->getBySymbol($name);
+    }
+
+    /**
+     * @return AnalystEntity
+     */
+    private function extractAnalyst($sheetData)
+    {
+        if (empty($sheetData[1]['B'])) {
+            throw new \InvalidArgumentException('Analyst name not found');
+        }
+
+        if (empty($sheetData[2]['B'])) {
+            throw new \InvalidArgumentException('Analyst company not found');
+        }
+
+        $name = $sheetData[1]['B'];
+        $company = $sheetData[2]['B'];
+
+        /** TODO Extract/create analyst */
+        die($name . $company);
+    }
+
 }
--- src/Raeting/RaetingBundle/Service/FileManagement.php
+++ src/Raeting/RaetingBundle/Service/FileManagement.php
@@ -4,23 +4,27 @@ namespace Raeting\RaetingBundle\Service;
 
 class FileManagement
 {
-    public function __construct()
+
+    public function scanDir($dir)
     {
-    }
-    
-    public function scanDir($dir) 
-    { 
-        $root = scandir($dir); 
+        $root = scandir($dir);
         $result = array();
-        foreach($root as $value) 
-        { 
-            if($value === '.' || $value === '..') {continue;} 
-            if(is_file("$dir/$value")) {$result[]="$dir/$value";continue;} 
+        foreach ($root as $value) {
+            if ($value === '.' || $value === '..') {
+                continue;
+            }
+
+            if (is_file("$dir/$value")) {
+                $result[]="$dir/$value";
+                continue;
+            }
+
             $result[]=$value;
-        } 
-        return $result; 
-    } 
-    
+        }
+
+        return $result;
+    }
+
     public function moveFile($from, $to)
     {
         return rename($from, $to);
